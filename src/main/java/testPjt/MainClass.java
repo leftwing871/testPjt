7ed801326bf8ec08063ba7e2e2ddc660d7d3253a
ehlee <ehlee@megazone.com>
2019-06-28 14:54:12 +0900

    2nd

--- src/main/java/testPjt/MainClass.java
+++ src/main/java/testPjt/MainClass.java
@@ -1,10 +1,17 @@
 package testPjt;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class MainClass {
 
 	public static void main(String[] args) {
-		TranspotationWalk transpotationWalk = new TranspotationWalk();
-		transpotationWalk.move();
+//		TransportationWalk transpotationWalk = new TransportationWalk();
+//		transpotationWalk.move();
+		
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+		
+		TransportationWalk transportationWalk = ctx.getBean("tWalk", TransportationWalk.class);
+		transportationWalk.move();
 	}
 
 }
